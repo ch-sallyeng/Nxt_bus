72d84d094841865b8971f87cefb51c2d2802db87
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-02-04 11:23:08 -0800

    Change SearchPanes.js from stateful to stateless component

--- client/src/components/search_panes.jsx
+++ client/src/components/search_panes.jsx
@@ -4,39 +4,32 @@ import { Tab } from 'semantic-ui-react'
 import NewSearch from './new_search.jsx'
 import PastSearch from './past_searches.jsx'
 
-class SearchPanes extends Component {
-  constructor(props) {
-    super(props);
-  }
+const SearchPanes = ({ getPredictions, setDirectionLabel, setBusLabel, setStopLabel, updateLabelsOnPastSearch }) => {
 
-  render() {
-    const { getPredictions, setDirectionLabel, setBusLabel, setStopLabel, updateLabelsOnPastSearch } = this.props;
+  const panes = [
+    { menuItem: 'New Search', render: () =>
+      <Tab.Pane attached={false}>
+        <NewSearch
+          getPredictions={getPredictions}
+          setDirectionLabel={setDirectionLabel}
+          setBusLabel={setBusLabel}
+          setStopLabel={setStopLabel}
+        />
+      </Tab.Pane>},
+    { menuItem: 'Past Searches', render: () =>
+      <Tab.Pane attached={false}>
+        <PastSearch
+          getPredictions={getPredictions}
+          updateLabelsOnPastSearch={updateLabelsOnPastSearch}
+        />
+      </Tab.Pane>}
+  ]
 
-    const panes = [
-      { menuItem: 'New Search', render: () =>
-        <Tab.Pane attached={false}>
-          <NewSearch
-            getPredictions={getPredictions}
-            setDirectionLabel={setDirectionLabel}
-            setBusLabel={setBusLabel}
-            setStopLabel={setStopLabel}
-          />
-        </Tab.Pane>},
-      { menuItem: 'Past Searches', render: () =>
-        <Tab.Pane attached={false}>
-          <PastSearch
-            getPredictions={getPredictions}
-            updateLabelsOnPastSearch={updateLabelsOnPastSearch}
-          />
-        </Tab.Pane>}
-    ]
-
-    return (
-      <div>
-        <Tab menu={{ secondary: true, pointing: true }} panes={panes}/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes}/>
+    </div>
+  )
 }
 
 export default SearchPanes
