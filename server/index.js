f3ef1528023219001b46233d09ae386860bb7ebd
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-02-14 17:25:47 -0800

    Set up server file for deployment

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use('/records', require('./routes/records'));
 /******************* END: REQUEST HANDLERS **********************/
 
 const serverPort = process.env.PORT || 3002;
-const apiPort = Number(process.env.PORT) + 1 || 3001;
+const apiPort = 3001;
 
 app.listen(serverPort, function() {
   console.log(`app listening on port ${serverPort}!`);
